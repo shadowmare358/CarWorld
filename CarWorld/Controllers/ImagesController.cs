346762cacbe24e33cfc5a636c9e68c1b283e6334
shadowmare <shadowmare@windowslive.com>
2020-09-21 11:59:50 +0200

    first commit

---
+++ CarWorld/Controllers/ImagesController.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CarWorld.Data;
+using CarWorld.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Security.Claims;
+using Microsoft.Extensions.Hosting;
+
+namespace CarWorld.Controllers
+{
+    [Authorize]
+    public class ImagesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _env;
+        private readonly IHttpContextAccessor _contextHttp;
+
+
+        public ImagesController(ApplicationDbContext context, IWebHostEnvironment env, IHttpContextAccessor contextHttp)
+        {
+            _context = context;
+            _env = env;
+            _contextHttp = contextHttp;
+        }
+        // GET: Images
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.ImageModel.Include(i => i.User);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Images/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var imageModel = await _context.ImageModel
+                .Include(i => i.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (imageModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(imageModel);
+        }
+
+        // GET: Images/Create
+        public IActionResult Create()
+        {
+            ViewData["UserID"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Images/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Description,ImageFile")] ImageModel imageModel)
+        {
+            var userId = _contextHttp.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            imageModel.UserID = userId;
+            if (ModelState.IsValid)
+            {
+                string wwwRootPath = _env.WebRootPath;
+                string fileName = Path.GetFileNameWithoutExtension(imageModel.ImageFile.FileName);
+                string extension = Path.GetExtension(imageModel.ImageFile.FileName);
+                string fileStoreageName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+                imageModel.ImageName = fileStoreageName;
+                string path = Path.Combine(wwwRootPath + "/images/", fileStoreageName);
+                using (var fileStream = new FileStream(path, FileMode.Create))
+                {
+                    await imageModel.ImageFile.CopyToAsync(fileStream);
+                }
+                _context.Add(imageModel);
+                await _context.SaveChangesAsync();
+
+            }
+            return View(imageModel);
+        }
+
+        // GET: Images/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var imageModel = await _context.ImageModel.FindAsync(id);
+            if (imageModel == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserID"] = new SelectList(_context.Users, "Id", "Id", imageModel.UserID);
+            return View(imageModel);
+        }
+
+        // POST: Images/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,UserID,ImageFile")] ImageModel imageModel)
+        {
+            if (id != imageModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    string wwwRootPath = _env.WebRootPath;
+                    string fileName = Path.GetFileNameWithoutExtension(imageModel.ImageFile.FileName);
+                    string extension = Path.GetExtension(imageModel.ImageFile.FileName);
+                    string fileStoreageName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+                    imageModel.ImageName = fileStoreageName;
+                    string path = Path.Combine(wwwRootPath + "/images/", fileStoreageName);
+                    using (var fileStream = new FileStream(path, FileMode.Create))
+                    {
+                        await imageModel.ImageFile.CopyToAsync(fileStream);
+                    }
+                    _context.Update(imageModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ImageModelExists(imageModel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(imageModel);
+        }
+
+        // GET: Images/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var imageModel = await _context.ImageModel
+                .Include(i => i.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (imageModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(imageModel);
+        }
+
+        // POST: Images/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var imageModel = await _context.ImageModel.FindAsync(id);
+            var imagePath = Path.Combine(_env.WebRootPath, "images", imageModel.ImageName);
+            if (System.IO.File.Exists(imagePath))
+            {
+                System.IO.File.Delete(imagePath);
+            }
+            _context.ImageModel.Remove(imageModel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ImageModelExists(int id)
+        {
+            return _context.ImageModel.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ CarWorld/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using CarWorld.Models;
+
+namespace CarWorld.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<CarWorld.Models.ImageModel> ImageModel { get; set; }
+    }
+}
